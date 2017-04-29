c1060468b73713d989ee35e948a4a0b78658d1a7
Gab De Jesus <quillot2112@gmail.com>
2017-04-29 15:41:08 +0900

    Updated website
    >Different views, now has base html
    >Supports login, signup, and user permissions to add products

--- del3/catalog/views.py
+++ del3/catalog/views.py
@@ -8,18 +8,24 @@ from .forms import ProductForm
 
 def index(request):
 	if request.method == 'POST':
-		form = ProductForm(request.POST)
-		if form.is_valid():
-			product_name = form.cleaned_data.get('product_name')
-			product_price = form.cleaned_data.get('product_price')
-			product = Product(product_name=product_name, product_price=product_price)
-			product.save()
-			return HttpResponseRedirect(reverse('catalog:index'))
+		if request.user.is_staff: 
+			form = ProductForm(request.POST)
+			if form.is_valid():
+				product_name = form.cleaned_data.get('product_name')
+				product_price = form.cleaned_data.get('product_price')
+				product = Product(product_name=product_name, product_price=product_price)
+				product.save()
+				return HttpResponseRedirect(reverse('catalog:index'))
+		else:
+			return HttpResponse('Not allowed to add product')
 	product_list = Product.objects.all()
 	return render(request, 'catalog/index.html', {'product_list': product_list})
 
 def add_product(request):
-	return render(request, 'catalog/addproduct.html')
+	if request.user.is_staff:
+		return render(request, 'catalog/addproduct.html')
+	else:
+		return HttpResponseRedirect(reverse('catalog:index'))
 
 def delete_product(request, product_id):
 	product = Product.objects.get(pk=product_id)
--- del3/del3/views.py
+++ del3/del3/views.py
@@ -10,36 +10,41 @@ def index(request):
 	return render(request, 'del3/index.html')
 
 def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			# form.save()
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=password)
-			auth_login(request, user)
-			return HttpResponseRedirect(reverse('catalog:index'))
+	if not request.user.is_authenticated():
+		if request.method == 'POST':
+			form = SignUpForm(request.POST)
+			if form.is_valid():
+				form.save()
+				username = form.cleaned_data.get('username')
+				password = form.cleaned_data.get('password1')
+				user = authenticate(username=username, password=password)
+				auth_login(request, user)
+				return HttpResponseRedirect(reverse('catalog:index'))
+		else:
+			form = SignUpForm()
+		return render(request, 'del3/signup.html', {'form': form})
 	else:
-		form = SignUpForm()
-	return render(request, 'del3/signup.html', {'form': form})
+		return HttpResponseRedirect(reverse('index'))
 
 def login(request):
-	if request.method == 'POST':
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			# form.save()
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					auth_login(request, user)
-					return HttpResponseRedirect(reverse('index'))
+	if not request.user.is_authenticated():
+		if request.method == 'POST':
+			form = LoginForm(request.POST)
+			if form.is_valid():
+				username = form.cleaned_data.get('username')
+				password = form.cleaned_data.get('password')
+				user = authenticate(username=username, password=password)
+				if user is not None:
+					if user.is_active:
+						auth_login(request, user)
+						return HttpResponseRedirect(reverse('index'))
+					else:
+						return HttpResponse('User inactive')
 				else:
-					return HttpResponse('User inactive')
-			else:
-				return HttpResponseRedirect(reverse('signup'))
-	return render(request, 'del3/login.html')
+					return HttpResponseRedirect(reverse('signup'))
+		return render(request, 'del3/login.html')
+	else:
+		return HttpResponseRedirect(reverse('index'))
 
 def logout(request):
 	auth_logout(request)
